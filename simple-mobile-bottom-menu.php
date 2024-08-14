637176f94a2120811ba23931929068c0b6f9c996
Robiul Islam <therobiulislam12@gmail.com>
2024-08-15 04:17:43 +0600

    inittal commit

---
+++ simple-mobile-bottom-menu.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @author Robiul Islam <therobiulislam12@gmail.com>
+ * @since 1.0.0
+ * @link #
+ * 
+ * @wordpress plugin
+ * 
+ * Plugin Name:       Simple Mobile Bottom Menu
+ * Description:       Simple Mobile Bottom Menu plugin for smooth navigation with customizable styles.
+ * Plugin URI:        #
+ * Version:           1.0.0
+ * 
+ * Author:            Robiul Islam
+ * Author URI:        https://robiul-islam.netlify.app
+ * 
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path:       /languages
+ * Text Domain:      simple-mobile-bottom-menu
+ *
+ *
+ * Simple Mobile Bottom Menu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 or latter of the License, or
+ * any later version.
+ *
+ * Simple Mobile Bottom Menu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'therobiulislam12/simple-mobile-bottom-menu',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'therobiulislam12/simple-mobile-bottom-menu' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
