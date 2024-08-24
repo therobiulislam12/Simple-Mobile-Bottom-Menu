1ead2ab261acca6ee45dddddf58c079a5b01ecfa
Robiul Islam <therobiulislam12@gmail.com>
2024-08-25 02:35:45 +0600

    menu show done

--- includes/Admin/views/menu-style.view.php
+++ includes/Admin/views/menu-style.view.php
@@ -1,3 +1,16 @@
+<?php 
+
+    // $menus = wp_nav_menu(array(
+    //     'theme_location' => 'smbm-bottom-menu'
+    // ));
+    $menuLocations = get_nav_menu_locations(); 
+    $menuID = $menuLocations['smbm-bottom-menu'];
+
+    $smbm_menu = wp_get_nav_menu_items($menuID);
+
+    inspect($smbm_menu)
+
+?>
 <div class="wrap">
     <h1 class="wp-heading-inline">Menu Style</h1>
 </div>
--- includes/Frontend/views/design_1.view.php
+++ includes/Frontend/views/design_1.view.php
@@ -1,46 +1,29 @@
+<?php 
+
+    $menuLocations = get_nav_menu_locations(); 
+    $menuID = $menuLocations['smbm-bottom-menu'];
+
+    $smbm_menu = wp_get_nav_menu_items($menuID);
+    $current_url = get_permalink();
+
+?>
+
 <nav>
     <div class="nav-box">
         <ul class="nav-container">
-            <li class="nav__item active">
-                <a href="#Home" class="nav__item-link">
-                    <div class="nav__item-icon">
-                        <img src="http://wplearning.com/wp-content/uploads/2024/08/home.png" alt="home">
-                    </div>
-                    <span class="nav__item-text">Home</span>
-                </a>
-            </li>
-            <li class="nav__item">
-                <a href="#Profile" class="nav__item-link">
-                    <div class="nav__item-icon">
-                        <img src="http://wplearning.com/wp-content/uploads/2024/08/user.png" alt="home">
-                    </div>
-                    <span class="nav__item-text">Profile</span>
-                </a>
-            </li>
-            <li class="nav__item">
-                <a href="#Messages" class="nav__item-link">
-                    <div class="nav__item-icon">
-                        <img src="http://wplearning.com/wp-content/uploads/2024/08/speech-bubble.png" alt="home">
-                    </div>
-                    <span class="nav__item-text">Messages</span>
-                </a>
-            </li>
-            <li class="nav__item">
-                <a href="#Help" class="nav__item-link">
-                    <div class="nav__item-icon">
-                        <img src="http://wplearning.com/wp-content/uploads/2024/08/info.png" alt="home">
-                    </div>
-                    <span class="nav__item-text">Help</span>
-                </a>
-            </li>
-            <li class="nav__item">
-                <a href="#Settings" class="nav__item-link">
-                    <div class="nav__item-icon">
-                        <img src="http://wplearning.com/wp-content/uploads/2024/08/settings.png" alt="home">
-                    </div>
-                    <span class="nav__item-text">Settings</span>
-                </a>
-            </li>
+            <?php 
+                foreach($smbm_menu  as $menu): 
+                    $is_active = ($current_url == $menu->url) ? 'active' : '';
+            ?>
+                <li class="nav__item <?php echo esc_attr($is_active); ?>">
+                    <a href="<?php echo esc_url($menu->url) ?>" class="nav__item-link">
+                        <div class="nav__item-icon">
+                            <img src="http://wplearning.com/wp-content/uploads/2024/08/home.png" alt="<?php echo esc_attr($menu->title) ?>">
+                        </div>
+                        <span class="nav__item-text"><?php echo esc_html($menu->title) ?></span>
+                    </a>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </div>
 </nav>
